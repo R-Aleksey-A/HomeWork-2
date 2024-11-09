8c77f581218ff90f54518d5b985d4ce786fbb06d
huawei <romanec.aleksey@gmail.com>
2024-11-10 01:11:43 +0400

    HW2.2

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,17 @@ public class Main {
         var dog = 8;
         var cat = 3.6;
         var paper = 767789;
-        System.out.println("dog=" + dog + " Cat=" + cat + " paper=" + paper);
+        System.out.println("dog=" + dog + " cat=" + cat + " paper=" + paper);
 
         System.out.println("Задание 2");
-        var dog = (dog + 4);
+        var dogIncrease = (dog + 4);
         System.out.println("dog+4=" + dog);
-        var cat = (cat + 4);
+        var catIncrease = (cat + 4);
         System.out.println("cat+4=" + cat);
-        var paper = (paper + 4);
+        var paperIncrease = (paper + 4);
         System.out.println("paper+4=" + paper);
 
-        System.out.println("dog+4=" + dog + " cat+4=" + cat + " paper+4=" + paper);
+        System.out.println("dog+4=" + dogIncrease + " cat+4=" + catIncrease + " paper+4=" + paperIncrease);
 
 
     }
