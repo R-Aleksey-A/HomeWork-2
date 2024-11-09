e2685be15763f14f37d53c30403dbe82f82c0e38
huawei <romanec.aleksey@gmail.com>
2024-11-09 23:09:14 +0400

    HW2.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        var dog =8;
+        var cat =3.6;
+        var parpel=767789;
+        System.out.println("dog="+dog+" Cat="+cat+" parpel="+parpel);
     }
 }
