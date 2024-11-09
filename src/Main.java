75af68df2e89b54788452aa9dcf8bac197749af1
huawei <romanec.aleksey@gmail.com>
2024-11-09 23:53:58 +0400

    HW2.2

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,15 @@ public class Main {
         System.out.println("dog=" + dog + " Cat=" + cat + " parpel=" + parpel);
 
         System.out.println("Задание 2");
-        var Dog = (dog * 4);
+        var Dog = (dog + 4);
         System.out.println("dog*4=" + Dog);
         var Cat = (cat * 4);
         System.out.println("cat*4=" + Cat);
         var Parpel = (parpel * 4);
         System.out.println("parpel*4=" + Parpel);
 
-        System.out.println("dog*4=" + Dog + " cat*4=" + Cat + " parpel*4=" + Parpel);
+        System.out.println("dog+4=" + Dog + " cat+4=" + Cat + " parpel+4=" + Parpel);
+
+
     }
 }
